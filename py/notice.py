92800e139c20fb62926240ab1f03413d81133a3e
zx <zx@ZX>
2014-10-17 11:39:57 +0800

    notice and new stap

--- py/notice.py
+++ py/notice.py
@@ -3,6 +3,7 @@
 import gtk
 from datetime import datetime
 import time
+import os
 r = 1
 
 class waterwindow:
@@ -18,11 +19,16 @@ class waterwindow:
 		self.window.set_position(gtk.WIN_POS_CENTER)
 		self.window.set_focus_on_map(True)
 		self.window.connect("destroy", gtk.main_quit)
+		f_str = os.popen("fortune").read()
+		self.button_f = gtk.Button(f_str)
+		self.button_f.set_size_request(400,100)
+		self.button_f.connect_object("clicked", gtk.Widget.destroy, self.window)
 		self.button = gtk.Button("Water Please~~~")
 		self.button.set_size_request(180,10)
 		self.button.connect_object("clicked", gtk.Widget.destroy, self.window)
 		self.bbox = gtk.HButtonBox()
-		self.bbox.pack_start(self.button, False, False, 0)
+		self.bbox.pack_start(self.button_f, False, False, 0)
+		self.bbox.pack_start(self.button, False, False, 1)
 		self.window.add(self.bbox)
 		self.window.show_all()
 		#self.button.show()
