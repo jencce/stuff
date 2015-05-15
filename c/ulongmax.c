6c3e7a02ba36314afc51135777ffa0c71d0a60c6
zx <zx@ZX>
2015-05-15 15:30:51 +0800

    ulong_max u_max

--- c/ulongmax.c
+++ c/ulongmax.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
 #define ULONG_MAX (~0UL)
+#define U_MAX (~0U)
 
 int main()
 {
 	printf("%lu\n", ULONG_MAX);
 	printf("%lx\n", ULONG_MAX);
+	printf("%u\n", ULONG_MAX);
+	printf("%x\n", ULONG_MAX);
+	printf("%lu\n", U_MAX);
+	printf("%lx\n", U_MAX);
+	printf("%u\n", U_MAX);
+	printf("%x\n", U_MAX);
 	return 0;
 }
