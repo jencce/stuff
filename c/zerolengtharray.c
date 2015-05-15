0ba384c271235e004aee1f5e8454252acb1b6853
zx <zx@ZX>
2015-05-15 15:13:58 +0800

    more pointer test

--- c/zerolengtharray.c
+++ c/zerolengtharray.c
@@ -21,13 +21,22 @@ int main()
 		struct node nodes[0];
 	};
 
+	struct root_s {
+		int fake;
+		int fake2;
+		struct node *nodes;  /* pointer */
+	};
+
 	struct root *hroot;
+	struct root_s *sroot;
 
 	printf("sizeof root: %d\n", sizeof(struct root));
-	printf("sizeof hroot: %d\n", sizeof(hroot));
+#if 1
+	/* zero-length array tests*/
+	printf("\nsizeof hroot: %d\n", sizeof(hroot));
+	hroot = (struct root *)malloc(sizeof(struct root)
+					+ 20*sizeof(struct node));
 
-	hroot = (struct root *)malloc(sizeof(struct root) +
-			20*sizeof(struct node));
 	printf("sizeof hroot: %d\n", sizeof(hroot));
 	printf("sizeof *hroot: %d\n", sizeof(*hroot));
 	printf("sizeof hroot->nodes: %d\n", sizeof(hroot->nodes));
@@ -43,11 +52,63 @@ int main()
 	printf("sizeof hroot->nodes[19]: %d\n", sizeof(hroot->nodes[19]));
 	printf("sizeof hroot->nodes[21]: %d\n", sizeof(hroot->nodes[21]));
 	printf("sizeof hroot->nodes[100]: %d\n", sizeof(hroot->nodes[100]));
-	printf("sizeof hroot->nodes[0].hash: %d\n", hroot->nodes[0].hash);
-	printf("sizeof hroot->nodes[19].hash: %d\n", hroot->nodes[19].hash);
-	printf("sizeof hroot->nodes[21].hash: %d\n", hroot->nodes[21].hash);
-	printf("sizeof hroot->nodes[100].hash: %d\n", hroot->nodes[100].hash);
+	printf("hroot->nodes[0].hash: %d\n", hroot->nodes[0].hash);
+	printf("hroot->nodes[19].hash: %d\n", hroot->nodes[19].hash);
+	printf("hroot->nodes[21].hash: %d\n", hroot->nodes[21].hash);
+	printf("hroot->nodes[100].hash: %d\n", hroot->nodes[100].hash);
 
 	free(hroot);
+#endif
+	/* pointer tests*/
+	printf("\nsizeof sroot: %d\n", sizeof(sroot));
+	sroot = (struct root_s *)malloc(sizeof(struct root_s));
+	sroot->nodes = (struct node *)malloc(20*sizeof(struct node));  /* complicate allocs */
+
+	printf("sizeof sroot: %d\n", sizeof(sroot));
+	printf("sizeof *sroot: %d\n", sizeof(*sroot));
+	printf("sizeof sroot->nodes: %d\n", sizeof(sroot->nodes));
+	printf("sizeof sroot->nodes[0]: %d\n", sizeof(sroot->nodes[0]));
+	printf("sizeof sroot->nodes[19]: %d\n", sizeof(sroot->nodes[19]));
+
+	printf("sizeof sroot->nodes[21]: %d\n", sizeof(sroot->nodes[21]));
+	printf("sizeof sroot->nodes[100]: %d\n", sizeof(sroot->nodes[100]));
+	sroot->nodes[0].hash = 0;
+	sroot->nodes[19].hash = 1;
+	/* write across the border will crash the upcoming frees */
+	//sroot->nodes[21].hash = 2;
+	//sroot->nodes[100].hash = 3;
+	printf("sizeof sroot->nodes[0]: %d\n", sizeof(sroot->nodes[0]));
+	printf("sizeof sroot->nodes[19]: %d\n", sizeof(sroot->nodes[19]));
+	printf("sizeof sroot->nodes[21]: %d\n", sizeof(sroot->nodes[21]));
+	printf("sizeof sroot->nodes[100]: %d\n", sizeof(sroot->nodes[100]));
+	printf("sroot->nodes[0].hash: %d\n", sroot->nodes[0].hash);
+	printf("sroot->nodes[19].hash: %d\n", sroot->nodes[19].hash);
+	printf("sroot->nodes[21].hash: %d\n", sroot->nodes[21].hash);
+	printf("sroot->nodes[100].hash: %d\n", sroot->nodes[100].hash);
+
+	free(sroot->nodes);
+	free(sroot);
+
+	/* init zero-length array ?  NO, this is NOT zla */
+	struct root_v {
+		int fake;
+		int fake2;
+		struct node nodes[5];
+	};
+
+	struct root_v vroot = {
+		.fake = 1,
+		.fake2 = 2,
+		.nodes[0] = {
+			.hash = 3,
+			.value = 4,
+		},
+	};
+
+	printf("\nsizeof rootv: %d\n", sizeof(struct root_v));
+	printf("sizeof vroot: %d\n", sizeof(vroot));
+	printf("vroot->nodes[0].hash: %d\n", vroot.nodes[0].hash);
+	printf("vroot->nodes[1].hash: %d\n", vroot.nodes[1].hash);
+
 	return 0;
 }
