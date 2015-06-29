63cb306ad66c4f6cf5f49a32ef563234a73ad556
zx <zx@ZX>
2015-06-29 13:50:40 +0800

    array length

--- py/args.py
+++ py/args.py
@@ -1,7 +1,9 @@
 #! /usr/bin/env python
 import sys
 
-print "arg0:{0},arg1:{1},arg2:{2}.".format(sys.argv[0],sys.argv[1],sys.argv[2])
+print "argc: {0}".format(sys.argv.__len__())
+print "argc: {0}".format(len(sys.argv))
+print "arg0:{0}, arg1:{1}, arg2:{2}.".format(sys.argv[0],sys.argv[1],sys.argv[2])
 
 data = 'qwe123hust'
 print data[0:6]
