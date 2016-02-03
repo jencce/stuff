2878c6affda6686ea68364323e4cf2439065200d
zx <zx@ZX>
2016-02-03 17:03:35 +0800

    0203

--- c/tsizeof.c
+++ c/tsizeof.c
@@ -2,13 +2,17 @@
 
 int main()
 {
-	printf("%d\n", sizeof (void ));  //1
-	printf("%d\n", sizeof (void *)); //4
-	printf("%d\n", sizeof (char)); //1
-	printf("%d\n", sizeof (short)); //2
-	printf("%d\n", sizeof (int)); //4
-	printf("%d\n", sizeof (long)); //4
-	printf("%d\n", sizeof (double)); //8
-	printf("%d\n", sizeof (long long)); //8
+	printf("sizeof (void ) %d\n", sizeof (void ));  //1
+	printf("sizeof (void *) %d\n", sizeof (void *)); //4
+	printf("sizeof (char) %d\n", sizeof (char)); //1
+	printf("sizeof (short) %d\n", sizeof (short)); //2
+	printf("sizeof (unsigned short) %d\n", sizeof (unsigned short)); //2
+	printf("sizeof (int) %d\n", sizeof (int)); //4
+	printf("sizeof (unsigned int) %d\n", sizeof (unsigned int)); //4
+	printf("sizeof (long) %d\n", sizeof (long)); //4
+	printf("sizeof (unsigned long) %d\n", sizeof (unsigned long)); //4
+	printf("sizeof (double) %d\n", sizeof (double)); //8
+	printf("sizeof (long long) %d\n", sizeof (long long)); //8
+	printf("sizeof (unsigned long long) %d\n", sizeof (unsigned long long)); //8
 	return 0;
 }
