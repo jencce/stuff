935ecf3e09c7cc81d942d7a0b27c2bd184c9e01c
Jencce Zhou <zx@zx>
2016-05-10 23:12:28 +0800

    __alignof__ struct

--- c/alignof.c
+++ c/alignof.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+struct node {
+	int num;
+	struct node * next;
+	long status;
+};
+
 int main()
 {
 	printf("__alignof__(int) = %d\n", __alignof__(int));
@@ -7,5 +13,6 @@ int main()
 	printf("__alignof__(unsigned long) = %d\n", __alignof__(unsigned long));
 	printf("__alignof__(float) = %d\n", __alignof__(float));
 	printf("__alignof__(double) = %d\n", __alignof__(double));
+	printf("__alignof__(struct node) = %d\n", __alignof__(struct node));
 	return 0;
 }
