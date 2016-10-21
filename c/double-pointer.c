78f41505551b331ff7668829566c8166e697dbb7
Jencce Zhou <zx@zx>
2016-10-21 23:05:43 +0800

    double pointer 2

--- c/double-pointer.c
+++ c/double-pointer.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 struct node {
 	int len;
 	char buf[10];
@@ -14,13 +16,23 @@ int main()
 	nodes[0] = node1;
 	nodes[1] = node2;
 
-	ss = &nodes;
-
-	s = *ss;
-	printf("len %d, s %s\n", s[0].len, s[0].buf);
+	//ss = &nodes;
+	//s = *ss;
+	//printf("len %d, s %s\n", s[0].len, s[0].buf);
 	//ss++;
 	//s = *ss;
 	//printf("len %d, s %s\n", s->len, s->buf);
+
+	s = (struct node *)malloc(2*sizeof(struct node));
+	memcpy(s, nodes, 2*sizeof(struct node));
+	printf("len %d, s %s\n", s->len, s->buf);
+	ss = &s;
+	printf("s %p *s %p, ss %p *ss %p **ss %p\n", s, *s, ss, *ss, **ss);
+	s++;
+	printf("s %p *s %p, ss %p *ss %p **ss %p\n", s, *s, ss, *ss, **ss);
+	printf("len %d, s %s\n", s->len, s->buf);
+	s--;
+	free(s);
 
 	return 0;
 }
